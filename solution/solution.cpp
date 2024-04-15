709c242b28e3eea6dfd5bf3c4d7b05231e51d4dc
QuestionCatcher <maciekp044@gmail.com>
2024-04-15 18:39:55 +0200

    few changes, new function

--- solution/solution.cpp
+++ solution/solution.cpp
@@ -1,32 +1,10 @@
-//system wiadmości
-//system tablicy do gry
-//system 2 graczy X oraz O
-
-
 #include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
-
-void newPlayer(string& playerName, char& symbol){
-    cout << "Player name: \n";
-    cin >> playerName;
-    cout << "Player symbol: \n";
-    cin >> symbol;
-};
-//function to choose Starting Player
-string chooseStartingPlayer(const string& player1, const string& player2){
-    int randomNum;
-    randomNum = rand() % 2;
-    if (randomNum == 0){
-        return player1;
-    } else  {
-        return player2;
-    }
-    
-}
-
 void createBoard() {
-    int board[9]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+    int board[9]{0, 1, 2, 3, 4, 5, 6, 7, 8};
 
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
@@ -35,34 +13,81 @@ void createBoard() {
         cout << "\n";
     }
 }
-string sendMessages(const string& situation)
-{
-    if (situation == "X_move")
-        return "Player X turn.";
-    else if (situation == "O_move")
-        return "Player O turn.";
-    else if (situation == "X_win")
-        return "Player X won the game, Congrats!";
-    else if (situation == "O_win")
-        return "Player O won the game, Congrats!";
-    else if (situation == "f_occupied")
-        return "This field is occupied. Select another.";
-    else
-        return "Unknown situation";
+void newPlayer(string& playerName, char& symbol){
+    cout << "Player name: \n";
+    cin >> playerName;
+    cout << "Player symbol: \n";
+    cin >> symbol;
+};
+//function to choose Starting Player
+string initializeWithRandomOneOrTwo() {
+        srand(static_cast<unsigned int>(time(nullptr))); // Seed the random generator
+        int randomNum = rand() % 2 + 1; // Generate random number 1 or 2
+        return to_string(randomNum);    // Convert int to string and return
+    }
+string chooseStartingPlayer(const string& player1, const string& player2){
+        string randomChoice = initializeWithRandomOneOrTwo();
+        if (randomChoice == "1"){
+            return player1;
+        } else
+        {
+            return player2;
+        }
 }
-
-int main(int argc, char* argv[])
+bool is_winner(string board[9])
 {
+    if (board[0] == board[1] && board[1] == board[2] && board[0] != " " ||
+        board[3] == board[4] && board[4] == board[5] && board[3] != " " ||
+        board[6] == board[7] && board[7] == board[8] && board[6] != " " ||
+        board[0] == board[3] && board[3] == board[6] && board[0] != " " ||
+        board[1] == board[4] && board[4] == board[7] && board[1] != " " ||
+        board[2] == board[5] && board[5] == board[8] && board[2] != " " ||
+        board[0] == board[4] && board[4] == board[8] && board[0] != " " ||
+        board[2] == board[4] && board[4] == board[6] && board[2] != " ")
+    {
+        return true;
+    }
+    return false;
+}
+int main(int argc, char* argv[]) {
     string player1Name, player2Name;
     char player1Symbol, player2Symbol;
 
     newPlayer(player1Name, player1Symbol);
     newPlayer(player2Name, player2Symbol);
 
-    string startingPlayer = chooseStartingPlayer(player1Name, player2Name);
+    const string startingPlayer = chooseStartingPlayer(player1Name, player2Name);
 
     cout << "Gracz " << startingPlayer << " rozpoczyna gre! \n";
     createBoard();
+
+    int currentPlayer = 1;
+    while (true) {
+        if (currentPlayer == 1) {
+            cout << "Ruch wykonuje " << player1Name << endl;
+            int x;
+            cout << "Podaj numer pola: \n";
+            cin >> x;
+            string board;
+            board[x] = player2Symbol;
+            
+            //zmiana gracza(chyba XD)
+            currentPlayer = 2;
+        } else {
+            cout << "Ruch wykonuje " << player2Name << endl;
+
+            int x;
+            cout << "Podaj numer pola: \n";
+            cin >> x;
+            string board;
+            board[x] = player2Symbol;
+            currentPlayer = 1;
+        }
+
+        //wyswietla tablice co ruch
+        createBoard();
+        //tutaj jakiś warunek (wygrany etc)
+    }
 
     return 0;
 }
